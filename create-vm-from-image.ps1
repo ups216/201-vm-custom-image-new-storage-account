ff49ebfb4f73012da1cfc08e7ae952b0284d2b04
Lei Xu <leixu216@gmail.com>
2017-08-15 16:45:04 +0800

    update url, deployment is successful

--- create-vm-from-image.ps1
+++ create-vm-from-image.ps1
@@ -1,3 +1,3 @@
-﻿New-AzureRmResourceGroup -Name "lx-vmfromimage02-rg" -Location "eastasia"
+﻿New-AzureRmResourceGroup -Name "lx-vmfromimage03-rg" -Location "eastasia"
 
-New-AzureRmResourceGroupDeployment -Name "myDeploymentName" -ResourceGroupName "lx-vmfromimage02-rg" -Mode Incremental -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json -Force -Verbose 
+New-AzureRmResourceGroupDeployment -Name "myDeploymentName" -ResourceGroupName "lx-vmfromimage03-rg" -Mode Incremental -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json -Force -Verbose 
