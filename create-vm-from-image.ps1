8eee0d7355b14b3dd7a11a31d41e2a868f95c9b0
Lei Xu <leixu216@gmail.com>
2017-08-15 15:50:38 +0800

    added template

---
+++ create-vm-from-image.ps1
@@ -0,0 +1,3 @@
+﻿New-AzureRmResourceGroup -Name "lx-vmfromimage02-rg" -Location "eastasia"
+
+New-AzureRmResourceGroupDeployment -Name "myDeploymentName" -ResourceGroupName "lx-vmfromimage02-rg" -Mode Incremental -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json -Force -Verbose 
