d5bd9ce458846eb0c5bf9f0d65982bab0365cd82
Lei Xu <leixu216@gmail.com>
2017-08-15 16:48:19 +0800

    script for create vm from image

---
+++ vm-from-image.ps1
@@ -0,0 +1,67 @@
+﻿$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+New-AzureRmResourceGroup -Name myResourceGroupFromImage -Location ChinaNorth
+
+$subnetConfig = New-AzureRmVirtualNetworkSubnetConfig `
+    -Name mySubnet `
+    -AddressPrefix 192.168.1.0/24
+
+$vnet = New-AzureRmVirtualNetwork `
+    -ResourceGroupName myResourceGroupFromImage `
+    -Location ChinaNorth `
+    -Name MYvNET `
+    -AddressPrefix 192.168.0.0/16 `
+    -Subnet $subnetConfig
+
+$pip = New-AzureRmPublicIpAddress `
+    -ResourceGroupName myResourceGroupFromImage `
+    -Location ChinaNorth `
+    -Name "mypublicdns$(Get-Random)" `
+    -AllocationMethod Static `
+    -IdleTimeoutInMinutes 4
+
+  $nsgRuleRDP = New-AzureRmNetworkSecurityRuleConfig `
+    -Name myNetworkSecurityGroupRuleRDP `
+    -Protocol Tcp `
+    -Direction Inbound `
+    -Priority 1000 `
+    -SourceAddressPrefix * `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 3389 `
+    -Access Allow
+
+  $nsg = New-AzureRmNetworkSecurityGroup `
+    -ResourceGroupName myResourceGroupFromImage `
+    -Location ChinaNorth `
+    -Name myNetworkSecurityGroup `
+    -SecurityRules $nsgRuleRDP
+
+$nic = New-AzureRmNetworkInterface `
+    -Name myNic `
+    -ResourceGroupName myResourceGroupFromImage `
+    -Location ChinaNorth `
+    -SubnetId $vnet.Subnets[0].Id `
+    -PublicIpAddressId $pip.Id `
+    -NetworkSecurityGroupId $nsg.Id
+
+$vmConfig = New-AzureRmVMConfig `
+    -VMName myVMfromImage `
+    -VMSize Standard_D1 | Set-AzureRmVMOperatingSystem -Windows `
+        -ComputerName myComputer `
+        -Credential $cred 
+
+# Here is where we create a variable to store information about the image 
+$image = Get-AzureRmImage `
+    -ImageName ls-win2016en-vs-image-20170814-01 `
+    -ResourceGroupName LX-WIN-TEMP02-RG
+
+# Here is where we specify that we want to create the VM from and image and provide the image ID
+$vmConfig = Set-AzureRmVMSourceImage -VM $vmConfig -Id $image.Id
+
+$vmConfig = Add-AzureRmVMNetworkInterface -VM $vmConfig -Id $nic.Id
+
+New-AzureRmVM `
+    -ResourceGroupName myResourceGroupFromImage `
+    -Location ChinaNorth `
+    -VM $vmConfig
